d61b09a0a1d4a4b2c60bfe8e49b1e68d2ddb9d32
heidy <heidyflorez2003@gmail.com>
2022-02-25 14:04:50 -0500

    ok

---
+++ PracticaPOO704/Persona.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaPOO704
+{
+    internal class Persona
+    {
+        private  string Nombre;
+        private int Edad;
+        private string Documento;
+
+        public Persona()
+        {
+
+        }
+        public Persona(string nombre, int edad, string documento)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Documento = documento;
+        }
+        public void SetNombre(string nombre)
+        {
+            Nombre = nombre;
+
+        }    
+
+        public void SetEdad(int edad)
+        {
+            Edad = edad;
+        }
+        
+        public void SetDocumento(string documento)
+        {
+            Documento = documento;
+        }
+        
+        public string ObtenerNombre()
+        {
+            return Nombre;
+        }
+
+        public int ObtenerEdad ()
+        {
+            return Edad;
+        }
+
+        public string ObtenerDocumento ()
+        {
+            return Documento;
+        }
+
+        public int Cumpleanios()
+        {
+            return Edad + 1;
+        }
+    }
+}
---
+++ PracticaPOO704/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PracticaPOO704
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+          
+            Persona sara = new ();
+            Console.WriteLine("\n Ingresa tu nombre");
+            sara.SetNombre(Console.ReadLine());
+
+            Console.WriteLine("\n Ingresa tu edad");
+            sara.SetEdad(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("\n Ingresa tu número de documento");
+            sara.SetDocumento(Console.ReadLine());
+
+            Console.WriteLine($"Su nombre es: {sara.ObtenerNombre()}, su edad es: {sara.ObtenerEdad()} y el número de identificación es: {sara.ObtenerDocumento()}.");
+
+            Console.WriteLine("Ingrese el nombre:");
+            var nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad:");
+            var edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el numero de documento:");
+            var documento = Console.ReadLine();
+
+            // se utiliza el contructor
+            var dony = new Persona(nombre, edad, documento);
+            Console.WriteLine("Su nombre es: " + dony.ObtenerNombre() + "Su edad es: " + dony.ObtenerEdad() + "Su documento es: " + dony.ObtenerDocumento());
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
